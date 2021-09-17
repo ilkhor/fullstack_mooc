8bd950b1ed29478bdda7c1413f29605e083fc7ee
Ilkka Horsma <ilkka.horsma@miiliconsulting.com>
2021-09-17 21:26:22 +0300

    1.3: kurssitiedot step3

--- osa1/part1/src/Content.js
+++ osa1/part1/src/Content.js
@@ -3,9 +3,9 @@ import Part from './Part';
 const Content = (props) => {
   return (
       <div>
-        <Part part={props.part1} content={props.exercises1} />
-        <Part part={props.part2} content={props.exercises2} />
-        <Part part={props.part3} content={props.exercises3} />
+        <Part part={props.part1.name} content={props.part1.exercises} />
+        <Part part={props.part2.name} content={props.part2.exercises} />
+        <Part part={props.part3.name} content={props.part3.exercises} />
       </div>
   );
 };
