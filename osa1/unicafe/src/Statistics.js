44d130f0917fba67b73de57e6abef4e309936a7e
Ilkka Horsma <ilkka.horsma@miiliconsulting.com>
2021-09-17 22:44:12 +0300

    1.7: unicafe step4

--- osa1/unicafe/src/Statistics.js
+++ osa1/unicafe/src/Statistics.js
@@ -1,33 +1,42 @@
 import Header from './Header';
 
-const Feedback = ({rating, value}) => (<p>{rating}: {value}</p>)
+const Feedback = ({rating, value}) => ( <p>{ rating }: { value }</p> );
 
 const Statistics = ({feedback}) => {
 
   const feedbackCount = () => feedback.good + feedback.neutral + feedback.bad;
   const average = () => {
-    const value = feedback.good-feedback.bad;
-    if ( value === 0) return 0;
+    const value = feedback.good - feedback.bad;
+    if (value === 0) return 0;
 
-    return (feedback.good-feedback.bad) / feedbackCount();
-  }
+    return ( feedback.good - feedback.bad ) / feedbackCount();
+  };
 
   const positive = () => {
     if (feedback.good === 0) return '0 %';
     const val = feedback.good / feedbackCount() * 100;
     return val + ' %';
+  };
+
+  if (feedbackCount() === 0) {
+    return (
+        <div>
+          <Header heading="Statistics"/>
+          <p>No feedback given</p>
+        </div> );
   }
 
   return (
-      <diV>
+
+      <div>
         <Header heading="Statistics"/>
-        <Feedback rating='good' value={feedback.good} />
-        <Feedback rating='neutral' value={feedback.neutral} />
-        <Feedback rating='bad' value={feedback.bad} />
-        <Feedback rating='total' value={feedbackCount()} />
-        <Feedback rating='average' value={average()} />
-        <Feedback rating='positive' value={positive()} />
-      </diV>
+        <Feedback rating="good" value={ feedback.good }/>
+        <Feedback rating="neutral" value={ feedback.neutral }/>
+        <Feedback rating="bad" value={ feedback.bad }/>
+        <Feedback rating="total" value={ feedbackCount() }/>
+        <Feedback rating="average" value={ average() }/>
+        <Feedback rating="positive" value={ positive() }/>
+      </div>
   );
 };
 
