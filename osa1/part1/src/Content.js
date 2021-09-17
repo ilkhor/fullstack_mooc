93436b7cce27c6134142cd7b1f1a0910102d5e51
Ilkka Horsma <ilkka.horsma@miiliconsulting.com>
2021-09-17 21:31:33 +0300

    1.4: kurssitiedot step4

--- osa1/part1/src/Content.js
+++ osa1/part1/src/Content.js
@@ -3,9 +3,9 @@ import Part from './Part';
 const Content = (props) => {
   return (
       <div>
-        <Part part={props.part1.name} content={props.part1.exercises} />
-        <Part part={props.part2.name} content={props.part2.exercises} />
-        <Part part={props.part3.name} content={props.part3.exercises} />
+        <Part part={props.parts[0].name} content={props.parts[0].exercises} />
+        <Part part={props.parts[1].name} content={props.parts[1].exercises} />
+        <Part part={props.parts[2].name} content={props.parts[2].exercises} />
       </div>
   );
 };
--- osa1/part1/src/Total.js
+++ osa1/part1/src/Total.js
@@ -1,7 +1,8 @@
 const Total = (props) => {
+  const parts = props.parts;
   return (
       <div>
-        <p>Number of exercises {props.total}</p>
+        <p>Number of exercises { parts.map(p => p.exercises).reduce((a, b) => a + b) }</p>
       </div>
   );
 };
