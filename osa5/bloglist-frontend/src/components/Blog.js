5704fe322266920e619b1fd71c7ba1cac04d28b7
Ilkka Horsma <ilkka.horsma@miiliconsulting.com>
2021-09-23 15:17:19 +0300

    5.7* blogilistan frontend, step7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,49 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author} {blog.url}
-  </div>
-)
+import React, { useState } from 'react';
 
-export default Blog
+const Blog = ({blog}) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const [state, setState] = useState('small');
+
+  const btnTxt = () => ( state === 'small' ? 'Näytä' : 'Piilota' );
+
+  const smallVisibility = () => ( {display: state === 'small' ? '' : 'none'} );
+  const bigVisibility = () => ( {display: state === 'big' ? '' : 'none'} );
+
+  const onClick = () => {
+    if (state === 'small') setState('big');
+    else setState('small');
+  };
+
+  const likes = () => {
+    if (blog.likes === undefined) return 0;
+    return blog.likes;
+  };
+
+  return (
+      <div style={ blogStyle }>
+        <div style={ smallVisibility() }>
+          <p>{ blog.title }</p>
+        </div>
+        <div style={ bigVisibility() }>
+          <p>{ blog.title }</p>
+          <p>{ blog.url }</p>
+          <div>Likes { likes() }
+            <button>Like</button>
+          </div>
+          <p>{ blog.author }</p>
+        </div>
+        <button onClick={ onClick }>{ btnTxt() }</button>
+      </div>
+  );
+
+};
+
+export default Blog;
