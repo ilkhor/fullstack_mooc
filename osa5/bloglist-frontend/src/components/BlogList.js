cc1e7b4eade8af8935a00c6dc6af39e107f75b8f
Ilkka Horsma <ilkka.horsma@miiliconsulting.com>
2021-09-24 10:43:32 +0300

    5.9*: blogilistan frontend, step9

--- osa5/bloglist-frontend/src/components/BlogList.js
+++ osa5/bloglist-frontend/src/components/BlogList.js
@@ -5,7 +5,12 @@ const BlogList = ({blogs, likeBlog}) => {
       <div>
         <h2>Blogs</h2>
         {
-          blogs.map(blog => <Blog key={ blog.id } blog={ blog } likeBlog={likeBlog}/>)
+          blogs.sort((a, b) => {
+            if (a.likes > b.likes) return -1;
+            else if (a.likes === b.likes) return 0;
+            return 1;
+          })
+          .map(blog => <Blog key={ blog.id } blog={ blog } likeBlog={ likeBlog }/>)
         }
       </div>
   );
