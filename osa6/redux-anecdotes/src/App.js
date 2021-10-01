51a35a84973d34b421db4e2312ae601398df2398
Ilkka Horsma <ilkka.horsma@miiliconsulting.com>
2021-10-01 23:47:26 +0300

    6.19 anekdootit ja connect, step1

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
-import Notification from './components/Notification';
+import ConnectedNotification from './components/Notification';
 import Filter from './components/Filter';
 import { useDispatch } from 'react-redux';
 import { initAnecdotes } from './reducers/anecdoteReducer';
@@ -15,7 +15,7 @@ const App = () => {
 
   return (
     <div>
-      <Notification/>
+      <ConnectedNotification/>
       <Filter/>
       <AnecdoteList/>
       <AnecdoteForm/>
--- osa6/redux-anecdotes/src/components/Notification.js
+++ osa6/redux-anecdotes/src/components/Notification.js
@@ -1,25 +1,30 @@
+import React from 'react';
+import { connect, useSelector } from 'react-redux';
 
-import React from 'react'
-import { useSelector } from 'react-redux';
-
-const Notification = () => {
+const Notification = (props) => {
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
-  }
+  };
 
-  const notification = useSelector(state => state.notification);
+  const notification = props.notification;
 
   if (notification === undefined || notification === null || notification.length === 0) {
     return null;
   }
 
   return (
-    <div style={style}>
-      {notification}
+    <div style={ style }>
+      { notification }
     </div>
-  )
-}
+  );
+};
 
-export default Notification
+const mapStateToProps = (state) => {
+  return { notification: state.notification };
+};
+
+const ConnectedNotification = connect(mapStateToProps)(Notification);
+
+export default ConnectedNotification;
